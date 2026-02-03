ed8455b80338f86de03601ae65cceb023d2b4e02
awesomeness2000 <Davidnoh0513@gmail.com>
2026-02-03 16:30:12 -0500

    Started building pages

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector("#site-nav");
+// Fade in on page load
+document.body.style.opacity = '1';
 
-if (navToggle && nav) {
-  navToggle.addEventListener("click", () => {
-    const isOpen = nav.classList.toggle("is-open");
-    navToggle.setAttribute("aria-expanded", String(isOpen));
-  });
+// Fade in projects
+const projectsTitle = document.querySelector('#projects .section-title');
+const filmsCard = document.querySelector('.project-card:first-child')
+const photosCard = document.querySelector('.project-card:nth-child(2)')
+const XRCard = document.querySelector('.project-card:nth-child(3)')
 
-  document.addEventListener("click", (e) => {
-    if (!nav.classList.contains("is-open")) return;
-    const inside = nav.contains(e.target) || navToggle.contains(e.target);
-    if (!inside) {
-      nav.classList.remove("is-open");
-      navToggle.setAttribute("aria-expanded", "false");
+function projectsFadeIn(e) {
+    if (window.scrollY > 300) {
+        projectsTitle.style.opacity = '1';
     }
-  });
+
+    if (window.scrollY > 500) {
+        filmsCard.style.opacity = '1';
+            setTimeout(() => {
+                photosCard.style.opacity = '1';
+        }, 500);
+            setTimeout(() => {
+                XRCard.style.opacity = '1';
+        }, 1000);
+    }
+
+
 }
+
+window.addEventListener('scroll', projectsFadeIn);
